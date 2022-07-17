9a65bb42b738cd90312459ac0fb4f87e37b3ded6
MCCZ <72745682+MCCZik@users.noreply.github.com>
2022-07-17 17:22:39 +0200

    Add files via upload

--- simulator/js.js
+++ simulator/js.js
@@ -28,8 +28,7 @@ var zre;
 var prikaz;
 var increment = 1;
 var muzuzen = 0;
-var homo = 10;
-var zena = 10;
+
 
 // Stats lidí
 var mi = [50,50,50,50,50];
@@ -77,41 +76,21 @@ function NextDay() {
     for (let i = 0; i < increment; i++) {
         document.getElementById('events').innerHTML += "<h2>" + Days + ". den</h2>"
         for (let i = 0; i < muzuzen; i++) {
-            homo = Math.floor(Math.random()*100+1);
-            if (homo > 20 && muzu >= 2) {
-                vybranymuz = Math.floor(Math.random()*muzu+1);
-                vybranazena = Math.floor(Math.random()*muzu+1);
-                if (vybranazena == vybranymuz) {
-                    vybranazena -= 1;
-                }
-                akce = Math.floor(Math.random()*(akci.length));
-                mreakce = Math.floor(Math.random()*(mreakci.length));
-                zreakce = Math.floor(Math.random()*(mreakci.length));
-                m = document.getElementById(vybranymuz + "m").value;
-                z = document.getElementById(vybranazena + "m").value;
-                a = akci[akce];
-                mre = mreakci[mreakce];
-                zre = mreakci[zreakce];
-                document.getElementById('events').innerHTML += m + a + z + "<br>";
-                document.getElementById('events').innerHTML += m + mre + "<br>";
-                document.getElementById('events').innerHTML += z + zre + "<br>"; 
-            } else {
-                vybranymuz = Math.floor(Math.random()*muzu+1);
-                vybranazena = Math.floor(Math.random()*zen+1);
-                akce = Math.floor(Math.random()*(akci.length));
-                mreakce = Math.floor(Math.random()*(mreakci.length));
-                zreakce = Math.floor(Math.random()*(zreakci.length));
-                m = document.getElementById(vybranymuz + "m").value;
-                z = document.getElementById(vybranazena + "z").value;
-                a = akci[akce];
-                mre = mreakci[mreakce];
-                zre = zreakci[zreakce];
-                document.getElementById('events').innerHTML += m + a + z + "<br>";
-                document.getElementById('events').innerHTML += m + mre + "<br>";
-                document.getElementById('events').innerHTML += z + zre + "<br>";      
-            }
-            Days = Days +1;       
+            vybranymuz = Math.floor(Math.random()*muzu+1);
+            vybranazena = Math.floor(Math.random()*zen+1);
+            akce = Math.floor(Math.random()*(akci.length));
+            mreakce = Math.floor(Math.random()*(mreakci.length));
+            zreakce = Math.floor(Math.random()*(zreakci.length));
+            m = document.getElementById(vybranymuz + "m").value;
+            z = document.getElementById(vybranazena + "z").value;
+            a = akci[akce];
+            mre = mreakci[mreakce];
+            zre = zreakci[zreakce];
+            document.getElementById('events').innerHTML += m + a + z + "<br>";
+            document.getElementById('events').innerHTML += m + mre + "<br>";
+            document.getElementById('events').innerHTML += z + zre + "<br>";      
         }
+        Days = Days +1;       
     }
 }
 
@@ -131,10 +110,9 @@ function KD(e) {
 
 function KDM(e) {
     if (e.key == "Enter") {
-        NextDay();
+        NextDay()
     }
 }
-
 function Prikaz() {
     prikaz = document.getElementById('cmds').value;
     if (prikaz.startsWith('days')) {
