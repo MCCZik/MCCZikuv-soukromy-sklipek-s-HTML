d7472006194d119e4feae56be00fc45f63e540e2
MCCZ <72745682+MCCZik@users.noreply.github.com>
2022-10-23 10:44:04 +0200

    Delete 291121 directory

--- 291121/README.md
+++
@@ -1,4 +0,0 @@
-# Create a Registration and Login System with PHP and MySQL
-Create a Registration and Login System with PHP and MySQL
-
-Article: https://speedysense.com/create-registration-login-system-php-mysql
--- 291121/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Enter your host name, database username, password, and database name.
-    // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("sql11.freemysqlhosting.net","sql11455036","h2B3ir1xfk","sql11455036");
-    // Check connection
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-?>
